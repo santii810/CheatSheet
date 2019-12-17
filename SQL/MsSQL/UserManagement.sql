312b04febe4ebc368e159c0e948135e75edf51c3
Santiago Gómez <santii810@hotmail.com>
2019-12-17 12:28:43 +0100

    Update UserManagement.sql

--- SQL/MsSQL/UserManagement.sql
+++ SQL/MsSQL/UserManagement.sql
@@ -1,12 +1,13 @@
 -- Login único por servidor
-CREATE LOGIN opt WITH PASSWORD = 'abc123..';
-ALTER LOGIN opt WITH CHECK_POLICY = OFF;
-ALTER LOGIN opt WITH PASSWORD = 'opt';
+CREATE LOGIN my_login WITH PASSWORD = 'abc123..';
+ALTER LOGIN my_login WITH CHECK_POLICY = OFF;
+ALTER LOGIN my_login WITH PASSWORD = 'new_password';
 -- User distinto en cada base de datos
-CREATE USER opt FOR LOGIN opt;  
-GRANT ALL TO opt;
+CREATE USER my_login FOR LOGIN my_login;  
+GRANT ALL TO my_login;
 
 
+--Lista usuarios
 select * from master.sys.server_principals
-
+--Lista usuarios quitando algunos de sistema
 SELECT * FROM sys.database_principals WHERE type_desc = 'SQL_USER' AND default_schema_name = 'dbo';
