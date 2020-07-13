7197e887e477a4464924f8b572f99742ce4f4576
Santiago Gómez <santii810@hotmail.com>
2020-07-13 13:04:48 +0200

    Create Tests postman.js

---
+++ Postman/Tests postman.js
@@ -0,0 +1,11 @@
+tests["Status code is 200"] = responseCode.code === 200;
+
+var data = JSON.parse(responseBody);
+
+
+var itemSincronismo = data.items.filter(item => item.name == 'MEDIDA-CTO')[0]
+console.log(itemSincronismo);
+
+tests["Sincronismo OK"] = itemSincronismo.status == "Test OK";
+tests["Puerto OK"] = itemSincronismo.oltPort < 17;
+tests["Slot bien"] = itemSincronismo.oltCard < 7;
